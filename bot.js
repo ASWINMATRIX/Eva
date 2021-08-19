6fb9fa5c359e89843d27fd9a7c6b516ea2a65f66
Aswin-Matrix <87300673+ASWINMATRIX@users.noreply.github.com>
2021-08-19 12:04:46 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -258,7 +258,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '💝______```ElsaMwol V2```_____💝' +
+                                await conn.sendMessage(conn.user.jid, '💝______```Jacki```_____💝' +
                                     '\n\n*🙍 ' + error + '*\n'
                                     , MessageType.text);
                             }
